3f4a62268701525385ff6924eaf702da9d95e9bc
stuseki <instusek@gmail.com>
2026-03-22 23:04:48 -0700

    feat: add login functionality

--- app/app.py
+++ app/app.py
@@ -1,9 +1,11 @@
 '''
 Semantic Retrieval System API
 '''
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from flask_sqlalchemy import SQLAlchemy
 import os
+from datetime import datetime, timedelta
+import secrets
 
 app = Flask(__name__)
 
@@ -14,6 +16,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+tomorrow = datetime.now() + timedelta(days=1)
 
 # Database Models
 
@@ -27,8 +31,8 @@ class User(db.Model):
 class Session(db.Model):
     __tablename__ = 'sessions'
     
-    session_id = db.Column(db.Integer, primary_key=True)
-    session_expiration = db.Column(db.DateTime)
+    session_id = db.Column(db.String(40), primary_key=True)
+    session_exp = db.Column(db.DateTime, default=tomorrow)
     
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
 
@@ -48,17 +52,52 @@ def create_user():
         db.session.add(user)
         db.session.commit()
     
+        #retrieving generated user_id
         get_user = db.session.query(User).filter_by(username=user.username).first()
     
         return jsonify(
             message="User created successfully",
             user_id=get_user.user_id
-        )
+        ), 200
+
     except:
         return jsonify(
             error="Username already exists"
-        )
+        ), 409
+
+
+@app.route('/auth/login', methods=['POST'])
+def login_user():
     
+    data = request.get_json()
+    
+    try:
+        #check entered user and password against database, create session if match
+        get_user = db.session.query(User).filter_by(username=data['username']).first()
+        if (get_user.password == data['password']):
+        
+            session_token = secrets.token_urlsafe(16) #generate unique session token
+        
+            session = Session(
+                session_id = session_token,
+                user_id = get_user.user_id
+            )
+    
+            db.session.add(session)
+            db.session.commit()
+        
+            return jsonify(
+                token=session_token,
+                user_id=get_user.user_id
+            ), 200
+        else:
+            return jsonify(
+                error="Invalid credentials"
+            ), 401
+    except:
+        return jsonify(
+            error="Invalid credentials"
+        ), 401
     
 # Database initialization
 
